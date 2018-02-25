eb9610cc471d2fc8951ea00f0dc27bbfc8bf0bdf
kodi4ek <kodi4ek@gmail.com>
2018-02-25 15:54:10 +0200

    Создали змейку и заставили двигаться.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -10,13 +11,45 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point( 1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Snake.Point( 4, 5, '#');
-            p2.Draw();
+            //Отрисовка рамки
+            HorizontalLine upline = new HorizontalLine( 0, 78, 0, '+');
+            VerticalLine leftline = new VerticalLine( 0, 24, 0, '+');
+            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
+            upline.Draw();
+            downline.Draw();
+            leftline.Draw();
+            rightline.Draw();
 
-            Console.ReadLine();
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+            snake.Move();
+            Thread.Sleep(100);
+            snake.Move();
+            Thread.Sleep(100);
+            snake.Move();
+            Thread.Sleep(100);
+            snake.Move();
+            Thread.Sleep(100);
+            snake.Move();
+            Thread.Sleep(100);
+            snake.Move();
+            Thread.Sleep(100);
+            snake.Move();
+            Thread.Sleep(100);
+            snake.Move();
+            Thread.Sleep(100);
+            snake.Move();
+            Thread.Sleep(100);
+            snake.Move();
+            Thread.Sleep(100);
+            snake.Move();
+            Thread.Sleep(100);
+
         }
     }
 }
